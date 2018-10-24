fbd3e154d8eb5be03e445922fecad5f39b45c1ed
user <user@DESKTOP-37GULAI>
2018-10-24 21:20:36 +0900

    JDH

--- job/src/main/java/job/member/controller/MemberController.java
+++ job/src/main/java/job/member/controller/MemberController.java
@@ -1,5 +1,21 @@
 package job.member.controller;
 
-public class MemberController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import job.member.controller.MemberService;
+
+@Controller
+public class MemberController {
+	@Autowired
+	private MemberService memberService;
+	
+	@RequestMapping(value="/main/index.do")
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index.jsp");
+		return modelAndView;
+	}
 }
--- job/src/main/java/job/member/dao/MemberDAO.java
+++ job/src/main/java/job/member/dao/MemberDAO.java
@@ -1,5 +1,43 @@
 package job.member.dao;
 
-public class MemberDAO {
+import java.util.HashMap;
+import java.util.Map;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import job.member.bean.MemberDTO;
+
+@Repository
+public class MemberDAO {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	public int write(MemberDTO memberDTO) {
+		return sqlSession.insert("mybatis.memberMapper.memberInsert", memberDTO);
+	}	
+	public int modify(MemberDTO memberDTO) {
+		return sqlSession.update("mybatis.memberMapper.memberModify", memberDTO);
+	}	
+	// 회원1명의 정보 읽어오기
+	public MemberDTO getMember(String id) {
+		return sqlSession.selectOne("mybatis.memberMapper.memberGet", id);
+	}
+	
+	public String login(String id, String pwd) {
+		Map<String, String> map = new HashMap<>();
+		map.put("id", id);
+		map.put("pwd", pwd);
+		return sqlSession.selectOne("mybatis.memberMapper.memberLogin", map);
+	}
+	
+	
+	public boolean isExistId(String id) {
+		boolean exist = false;
+		if(sqlSession.selectOne("mybatis.memberMapper.isExistId", id) != null) {
+			exist = true;
+		}
+		return exist;
+	}
 }
