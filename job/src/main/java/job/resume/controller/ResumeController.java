81026ee5cd3148ece1a0330416dbdd6b098ddfaa
eonesk <alzl1004132@naver.com>
2018-10-29 21:33:01 +0900

    21:31

--- job/src/main/java/job/resume/controller/ResumeController.java
+++ job/src/main/java/job/resume/controller/ResumeController.java
@@ -1,6 +1,9 @@
 package job.resume.controller;
 
 import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -34,12 +37,13 @@ public class ResumeController {
 		return modelAndView;
 	}
 	
+	@RequestMapping(value = "/job/resume/resume/resumeWrite.do")
 	public ModelAndView resumeWrite(HttpServletRequest request) {
 		ModelAndView modelAndView = new ModelAndView();
 		HttpSession session = request.getSession();
-		String[] tableName = {"rsim", "rss", "rsw", "rsit", 
-				"rse", "rsls", "rst", "rsf", 
-				"rslg", "rspf", "rsv", "rspr"};
+		String[] tableName = {"RSIM", "RSS", "RSW", "RSIT", 
+				"RSE", "RSLS", "RST", "RSF", 
+				"RSLG", "RSPF", "RSV", "RSPR"};
 		Integer[] seq = new Integer[tableName.length];
 		try {
 			request.setCharacterEncoding("utf-8");
@@ -49,23 +53,35 @@ public class ResumeController {
 		
 		for(int i=0; i<seq.length; i++) {
 			if(request.getParameter(tableName[i]+"Seq") != null) {
-				seq[i] = Integer.parseInt(request.getParameter(tableName[i]+"Seq"));
+				seq[i] = Integer.parseInt(request.getParameter(tableName[i]+"_seq"));
 			}else {
 				seq[i] = null;
 			}
 		}
-		
+		Date date=null;
+		try {
+			date = new SimpleDateFormat("yyyyMMdd").parse(request.getParameter("rs_birth"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		ResumeDTO resumeDTO = new ResumeDTO();
 		resumeDTO.setM_Id((String)session.getAttribute("memId"));
-		resumeDTO.setRs_Title(request.getParameter("title"));
-		resumeDTO.setRs_Type(request.getParameter("type"));
-		resumeDTO.setRs_Pay(request.getParameter("pay"));
-		resumeDTO.setRs_Workspace1(request.getParameter("workspace1"));
-		resumeDTO.setRs_Workspace2(request.getParameter("workspace2"));
-		resumeDTO.setRs_Workspace3(request.getParameter("workspace3"));
-		resumeDTO.setRs_Job1(request.getParameter("job1"));
-		resumeDTO.setRs_Job2(request.getParameter("job2"));
-		resumeDTO.setRs_Job3(request.getParameter("job3"));
+		resumeDTO.setRs_Name(request.getParameter("rs_name"));
+		resumeDTO.setRs_Birth(date);
+		resumeDTO.setRs_Gender(request.getParameter("rs_gender"));
+		resumeDTO.setRs_Email(request.getParameter("rs_gender"));
+		resumeDTO.setRs_Homenum(request.getParameter("rs_homenum"));
+		resumeDTO.setRs_Phone(request.getParameter("rs_phone"));
+		resumeDTO.setRs_Address(request.getParameter("rs_address"));
+		resumeDTO.setRs_Title(request.getParameter("rs_title"));
+		resumeDTO.setRs_Pay(request.getParameter("rs_pay"));
+		resumeDTO.setRs_Type(request.getParameter("rs_type"));
+		resumeDTO.setRs_Workspace1(request.getParameter("rs_workspace1"));
+		resumeDTO.setRs_Workspace2(request.getParameter("rs_workspace2"));
+		resumeDTO.setRs_Workspace3(request.getParameter("rs_workspace3"));
+		resumeDTO.setRs_Job1(request.getParameter("rs_job1"));
+		resumeDTO.setRs_Job2(request.getParameter("rs_job2"));
+		resumeDTO.setRs_Job3(request.getParameter("rs_job3"));
 		resumeDTO.setRsim_Seq(seq[0]);
 		resumeDTO.setRss_Seq(seq[1]);
 		resumeDTO.setRsw_Seq(seq[2]);
